8553c37a53b1fa8e87a0dd1651e29b8e8b3e9686
Valerij575 <valerij575@gmail.com>
2021-08-24 20:20:40 +0300

    Add Domain DbConnection

--- CleanOnionArchitecture/CleanOnionArchitecture.Web/Startup.cs
+++ CleanOnionArchitecture/CleanOnionArchitecture.Web/Startup.cs
@@ -1,17 +1,12 @@
+using CleanOnionArchitecture.Infra.Data.Context;
 using CleanOnionArchitecture.Web.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace CleanOnionArchitecture.Web
 {
@@ -27,9 +22,13 @@ namespace CleanOnionArchitecture.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ProjectDbContext>(options => 
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("ProjectConnection")));
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
+
             services.AddDatabaseDeveloperPageExceptionFilter();
 
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
