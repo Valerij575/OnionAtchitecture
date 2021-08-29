971ca9ae490de96cfedc8d5dc53073bf78e72b74
Valerij575 <valerij575@gmail.com>
2021-08-30 01:05:14 +0300

    Add Inversion of Control and SendGrid

---
+++ CleanOnionArchitecture/CleanArchitecture.Application/Interfaces/IEmailSender.cs
@@ -0,0 +1,11 @@
+﻿using SendGrid;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Application.Interfaces
+{
+    public interface IEmailSender
+    {
+        Task<Response> SendEmailAsync(string email, string subject, string message);
+        Task<Response> Execute(string apiKey, string subject, string message, string email);
+    }
+}
---
+++ CleanOnionArchitecture/CleanOnionArchitecture.Infra.Ioc/DependencyContainer.cs
@@ -0,0 +1,18 @@
+﻿using CleanArchitecture.Application.Interfaces;
+using CleanArchitecture.Application.Services;
+using CleanArchitecture.Domain.Interfaces;
+using CleanOnionArchitecture.Infra.Data.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanOnionArchitecture.Infra.Ioc
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            services.AddScoped<IProjectService, ProjectService>();
+            services.AddScoped<IProjectRepository, ProjectRepository>();
+            services.AddTransient<IEmailSender, EmailSender>();
+        }
+    }
+}
